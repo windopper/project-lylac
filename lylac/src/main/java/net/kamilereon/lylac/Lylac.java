c5eee84e2a31862ae2a0a4b11c1876f77b7360ce
windopper <open0916@naver.com>
2022-10-23 13:48:57 +0000

    .

---
+++ lylac/src/main/java/net/kamilereon/lylac/Lylac.java
@@ -0,0 +1,16 @@
+package net.kamilereon.lylac;
+
+import java.util.EventListener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lylac extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+    @Override
+    public void onDisable() {
+        
+    }
+}
